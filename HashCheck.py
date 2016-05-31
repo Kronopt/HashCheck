6aebb2e82a15826addbb7d82078835cfbf704191
Kronopt <Kronopt@users.noreply.github.com>
2016-05-31 18:58:20 +0100

    Some corrections, refractorings and little code changes

--- HashCheck.py
+++ HashCheck.py
@@ -15,7 +15,7 @@ HOW TO RUN:
       Calculates md5 hashes for every file available in the current directory
       
     - Through the command line.
-      Allows for the hashing of single files or other entire directories;
+      Allows for the hashing of single files or other specific directories;
       Allows the selection of the hashing algorithm;
       For more help, call the script with the -h parameter
 '''
@@ -26,15 +26,15 @@ import os
 
 __author__ = 'Pedro HC David, https://github.com/Kronopt'
 __credits__ = ['Pedro HC David']
-__version__ = '0.2'
-__date__ = '20:50h, 27/05/2016'
+__version__ = '0.3'
+__date__ = '18:53h, 31/05/2016'
 __status__ = 'Production'
 
 
 
 
+# Verbose mode (time it takes to hash, number of files to hash, etc)
 # Feeding file a chunk at a time, instead of loading it as a whole to memory
-# When Hashing every file on the same directory, ask ipermission to proceed with hash if multiple "not dir" files exist
 # Allow multithreading
 # Allow to compare an input hash (returns true or false if hash is the same or not)
 # Allow relative path of files (relative to current directory)
@@ -42,35 +42,10 @@ __status__ = 'Production'
 
 
 
-def hashCheck(hashAlgorithm, fileName):
-    '''
-    Main function
-    Calculates the hash of 'fileName' using the hashing algorithm specified in 'hashAlgorithm'
-
-
-    hashAlgorithm: str, representing a hashing algorithm
-    fileName: str, representing a file path
-    
-    REQUIRES:
-        - Hashing algorithm available in the hashlib library
-        - Existing file 'fileName'
-    
-    ENSURES:
-        Hash of 'fileName' using 'hashAlgorithm'
-    '''
-    
-    with open(fileName, 'rb') as fileToCheck:
-        # defaults to md5
-	hashOutput = getattr(hashlib, hashAlgorithm, 'md5')(fileToCheck.read()).hexdigest()
-
-	# only prints the actual file name, not the whole path
-	# The "\" might fail on non windows operating systems...
-	print hashAlgorithm, "hash for '" + os.path.split(fileName.rstrip('\\'))[1] + "': " + hashOutput
-
-def multipleHashCheck(hashAlgorithm, directory):
+def main(hashAlgorithm, directory):
     '''
     Runs the hashCheck function for every file in a directory or for a single file
-    Checks wether 'directory' exists and if its  really a directory or just a file
+    Checks wether 'directory' exists and if it's really a directory or just a file
 
     
     hashAlgorithm: str, representing a hashing algorithm
@@ -93,15 +68,41 @@ def multipleHashCheck(hashAlgorithm, directory):
             filesToHash.sort()
 
             for i in filesToHash:
-                hashCheck(hashAlgorithm, i)
-
+                print hashAlgorithm, "hash for '" + i + "': " + hashCheck(hashAlgorithm, i)
+                
         elif os.path.isfile(directory):
             hashCheck(hashAlgorithm, directory)
+            # only prints the actual file name, not the whole path
+            print hashAlgorithm, "hash for '" + os.path.split(directory)[1] + "': " + \
+                  hashCheck(hashAlgorithm, directory)
 
         raw_input()
             
     else:
         print "'" + directory + "' does not exist..."
+
+def hashCheck(hashAlgorithm, fileName):
+    '''
+    Main function
+    Calculates the hash of 'fileName' using the hashing algorithm specified in 'hashAlgorithm'
+
+
+    hashAlgorithm: str, representing a hashing algorithm
+    fileName: str, representing a file path
+    
+    REQUIRES:
+        - Hashing algorithm available in the hashlib library
+        - Existing file 'fileName'
+    
+    ENSURES:
+        Hash of 'fileName' using 'hashAlgorithm'
+    '''
+    
+    with open(fileName, 'rb') as fileToCheck:
+        # defaults to md5
+        hashOutput = getattr(hashlib, hashAlgorithm, 'md5')(fileToCheck.read()).hexdigest()
+
+        return hashOutput
 
 if __name__ == '__main__':
     # Default = -hash md5 -file os.getcwd()
@@ -114,4 +115,4 @@ if __name__ == '__main__':
 
     parser = parser.parse_args()
     
-    multipleHashCheck(parser.hash, parser.file)
+    main(parser.hash, parser.file)
