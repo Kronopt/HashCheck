6da3f1e92452e0ed1a7082ed802115f5369d5130
Kronopt <kronopt@users.noreply.github.com>
2016-10-17 17:20:15 +0100

    Altered code to comply with PEP8 specifications

--- HashCheck.py
+++ HashCheck.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-'''
+"""
 HASH CHECK
 Calculates file hashes (single file or entire directories) using different hashing algorithms
 
@@ -11,12 +11,12 @@ DEPENDENCIES:
 HOW TO RUN:
     - Directly, by double clicking the script.
       Calculates md5 hashes for every file available in the current directory
-      
+
     - Through the command line.
       Allows for the hashing of single files or other specific directories;
       Allows the selection of the hashing algorithm;
       For more help, call the script with the -h parameter
-'''
+"""
 
 import argparse
 import hashlib
@@ -28,82 +28,85 @@ __version__ = '0.5'
 __date__ = '02:00h, 16/06/2016'
 __status__ = 'Production'
 
-def main(hashAlgorithm, directory):
-    '''
-    Runs the hashCheck function for every file in a directory or for a single file
+
+def main(hash_algorithm, directory):
+    """
+    Runs the hash_check function for every file in a directory or for a single file
     Checks whether 'directory' is really a directory or just a file
 
     PARAMETERS:
-        hashAlgorithm : str
+        hash_algorithm : str
             Represents a hashing algorithm
         directory : str
             Represents a file path or a directory
 
     REQUIRES:
         Hashing algorithm available in the hashlib library
-    
+
     ENSURES:
-        Hash of 'directory' using 'hashAlgorithm' if it's a file or hash of every file in
+        Hash of 'directory' using 'hash_algorithm' if it's a file or hash of every file in
         the directory if it's a directory
-    '''
+    """
 
     # Handles directories
     if os.path.isdir(directory):
-        filesToHash = filter(os.path.isfile, os.listdir(directory))
-        filesToHash.sort()
+        files_to_hash = filter(os.path.isfile, os.listdir(directory))
+        files_to_hash.sort()
 
-        for i in filesToHash:
-            print hashAlgorithm, "hash for '" + i + "': " + hashCheck(hashAlgorithm, i)
+        for i in files_to_hash:
+            print hash_algorithm, "hash for '" + i + "': " + hash_check(hash_algorithm, i)
 
     # Handles single files
     elif os.path.isfile(directory):
-        hashCheck(hashAlgorithm, directory)
+        hash_check(hash_algorithm, directory)
         # only prints the actual file name, not the whole path
-        print hashAlgorithm, "hash for '" + os.path.split(directory)[1] + "': " \
-              + hashCheck(hashAlgorithm, directory)
-        
+        print hash_algorithm, "hash for '" + os.path.split(directory)[1] + "': " \
+                              + hash_check(hash_algorithm, directory)
+
     else:
         print "'" + directory + "' does not exist..."
 
     raw_input()
 
-def hashCheck(hashAlgorithm, fileName):
-    '''
+
+def hash_check(hash_algorithm, file_name):
+    """
     Main function
-    Calculates the hash of 'fileName' using the hashing algorithm specified in 'hashAlgorithm'
+    Calculates hash of file_name using the hashing algorithm specified in hash_algorithm
 
     PARAMETERS:
-        hashAlgorithm : str
+        hash_algorithm : str
             Represents a hashing algorithm
-        fileName : str
+        file_name : str
             Represents a file path
-    
+
     REQUIRES:
         - Hashing algorithm available in the hashlib library
-        - Existing file 'fileName'
-    
+        - Existing file file_name
+
     ENSURES:
-        Hash of 'fileName' using 'hashAlgorithm'
-    '''
-    
-    with open(fileName, 'rb') as fileToCheck:
+        Hash of file_name using hash_algorithm
+    """
+
+    with open(file_name, 'rb') as file_to_check:
         # defaults to md5
-        hashOutput = getattr(hashlib, hashAlgorithm, 'md5')()
-        
-        for line in fileToCheck:
-            hashOutput.update(line)
-        
-        return hashOutput.hexdigest()
+        hash_output = getattr(hashlib, hash_algorithm, 'md5')()
+
+        for line in file_to_check:
+            hash_output.update(line)
+
+        return hash_output.hexdigest()
+
 
 if __name__ == '__main__':
     # Default = -hash md5 -file os.getcwd()
-    parser = argparse.ArgumentParser(description = 'Multiple hash algorithm check tool')
+    parser = argparse.ArgumentParser(description='Multiple hash algorithm check tool')
 
-    parser.add_argument('-hash', choices = hashlib.algorithms, default = 'md5',
-                        help = 'Available hash algorithms (chose one)')
-    parser.add_argument('-file', nargs = '?', const = os.getcwd(), default = os.getcwd(),
-                        metavar = 'dir', help = 'Directory or file path')
+    parser.add_argument('-hash', choices=hashlib.algorithms, default='md5',
+                        help='Available hash algorithms (chose one)')
+    parser.add_argument('-file', nargs='?', const=os.getcwd(), default=os.getcwd(),
+                        metavar='dir', help='Directory or file path')
 
     parser = parser.parse_args()
-    
+
     main(parser.hash, parser.file)
